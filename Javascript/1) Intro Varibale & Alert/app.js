5779ccb573d5f9c52239bae201f5a4583aa1ddfc
alihuzaifa <ha4587323@gmail.com>
2024-11-19 01:00:42 +0500

    dashboard layout with css hackathon website practice project and javascript intro has pushed or covered in class

---
+++ Javascript/1) Intro Varibale & Alert/app.js
@@ -0,0 +1,24 @@
+// alert('Hello World');
+
+
+// String =>  every characters except numbers '',"",``
+// Number => 1234567890
+
+
+
+// alert(10 / 10);
+// alert('Hello World');
+
+
+// var paragraph = 'Hello World';
+// alert(paragraph)
+
+// var number1 = 100;
+// var number2 = 300;
+// alert(number1 + number2);
+
+var greeting = 'Assalamu Alaikum ';
+var myName = 'Ali Huzaifa';
+
+alert(greeting + myName)
+

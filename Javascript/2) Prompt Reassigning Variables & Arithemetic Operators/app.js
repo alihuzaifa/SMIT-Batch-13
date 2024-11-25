0a6a52b35e3490884194736fbe4c5952b34a826d
alihuzaifa <ha4587323@gmail.com>
2024-11-25 11:15:37 +0500

    prompt reassigning of variables arithemetic operators has been covered in class

---
+++ Javascript/2) Prompt Reassigning Variables & Arithemetic Operators/app.js
@@ -0,0 +1,43 @@
+// var myName = "Amjad";
+
+// var studentMarks = 0;
+// studentMarks = 200;
+// alert(studentMarks);
+
+// var studentMarks;
+
+// studentMarks = "200";
+
+// alert(studentMarks);
+
+// var studentsProgress = "";
+// studentsProgress = "Good";
+
+// var alert = "100";
+
+// var my name
+
+// var _ $
+
+// var number1
+
+// var message = "The Total Cost is " + 10;
+// alert(10, 20);
+// console.log(10, 20);
+
+// var number1 = 10;
+// var number2 = 20;
+
+// var userFirstName = prompt("Enter Your First Name", "Ali");
+// console.log(userFirstName);
+
+// var number1 = +prompt("Enter First Number");
+// var number2 = +prompt("Enter Second Number");
+// console.log();
+
+// var number1 = 77;
+// var number2 = 9;
+// console.log(number1 % number2);
+
+// var userNumber = +prompt("Enter Your Number");
+// console.log(userNumber % 2);

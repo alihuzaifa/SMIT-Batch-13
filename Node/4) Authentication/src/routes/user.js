41a6247d8a9be199433edf99a2a317415f6ead5b
HasanCoder66 <hasanashraf874@gmail.com>
2025-08-29 22:35:08 +0500

    bcrypt jsonwebtoken intro with user creation

---
+++ Node/4) Authentication/src/routes/user.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { createUser, signinUser } from "../controllers/user.js";
+const router = express.Router();
+
+router.post("/signup", createUser);
+router.post("/login", signinUser);
+
+export default router;
